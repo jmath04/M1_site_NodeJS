e8279fab9c9693209dd08dd5469da5fe920263f4
João <jmatheusdeoliveira341@gmail.com>
2025-09-01 20:26:07 -0300

    iniciado tentativa de fazer pagina exclusiva usando user session

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const session = require('express-session');
 
 const app = express();
 
 const path = require('path');
+
 
 const users = [
     {
@@ -14,13 +16,10 @@ const users = [
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(express.static(path.join(__dirname,'front')));
-
 app.listen(3000);
 
-
-app.get('/', function(req,response){
-    response.sendFile('index.html');
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'front','index.html'));
 });
 
 app.post('/registrar', function(req,res){
@@ -29,12 +28,24 @@ app.post('/registrar', function(req,res){
     user = users.find( u => ( u.username == username));
 
     if(user && user.password == password){
+        req.session.user = {username : user.username};
         res.redirect('/logado');
     }
     else{
         res.send('usuário ou senha incorretos');
     }
 });
+
+
+app.get('/logado',(req,res) =>{
+    if(req.session.user){
+        res.sendFile(path.join(__dirname,'front','logado.html'));
+    }
+    else{
+        res.redirect('/');
+    }
+})
+
 
 
 
