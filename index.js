a0c7764e5b3a56c07d6965e2733e924d5492d32b
scheila <112344447+ScheilaCeresa@users.noreply.github.com>
2025-09-02 20:14:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'front','index.html'));
 });
 
+// teste
+
 app.post('/registrar', function(req,res){
     const {username, password} = req.body;
 
