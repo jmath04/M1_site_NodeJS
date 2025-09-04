3b766797666147c6c7243a0b27985e80c81699d0
João <jmatheusdeoliveira341@gmail.com>
2025-09-03 23:17:58 -0300

    finalizado o projeto

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookies = require('cookies')
 const app = express();
 
 const path = require('path');
+const Cookies = require('cookies');
 
 
 const users = [
@@ -21,17 +22,25 @@ app.use(cookieParser());
 app.listen(3000);
 
 app.get('/', function(req,res){
+    if(req.cookies.currentUser){
+        res.redirect('/logado');
+    }
     res.sendFile(path.join(__dirname,'front','index.html'));
 });
 
-app.post('/registrar', function(req,res){
-    const {username, password} = req.body;
+app.post('/login', function(req,res){
+    const {username, password, manter} = req.body;
 
-    user = users.find( u => ( u.username == username));
+    const user = users.find( u => ( u.username == username));
 
     if(user && user.password == password){
-        res.cookie('currentUser',user.username, {maxAge:90000000});
-        res.redirect('/logado');
+        if(manter === 1){
+            res.cookie('currentUser',user.username, {maxAge: 259200});
+            res.redirect('/logado');
+        }else{
+            res.cookie('currentUser',user.username);
+            res.redirect('/logado');
+        }
     }
     else{
         res.send('usuário ou senha incorretos');
@@ -41,11 +50,35 @@ app.post('/registrar', function(req,res){
 
 app.get('/logado',(req,res) =>{
     if(req.cookies.currentUser){
-        res.sendFile(path.join(__dirname,'front','logado.html'));
+        const nome = req.cookies.currentUser;
+        res.send(
+            `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Logado</title>
+            </head>
+            <body>
+            <h1>Ola ${nome} você está logado</h1>    
+            <form action='/logoff' id="logoff" method="post">
+            logoff
+            <button type="subimit" id="logoff">
+            </form>
+            </body>
+            </html>
+            `
+        );
     }
     else{
         res.redirect('/');
     }
+})
+
+app.all('/logoff', (req,res) =>{
+   res.clearCookie('currentUser');
+   res.redirect('/');
 })
 
 
